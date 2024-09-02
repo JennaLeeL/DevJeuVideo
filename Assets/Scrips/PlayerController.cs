c1d4af579cced1787a46962f375b274e81aae532
JennaLeeL <jennalee.lecavalier@gmail.com>
2024-09-02 17:50:10 -0400

    Cleaned Player controller script

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -10,10 +10,7 @@ public class PlayerController : MonoBehaviour
 
     private float horizontalInput;
     private float forwardInput;
-    
-    // Set Keys on Unity Up and Down Arrow
-    public KeyCode startKey = KeyCode.UpArrow; 
-    public KeyCode stopKey = KeyCode.DownArrow; 
+
 
     // Update is called once per frame
     void Update()
@@ -21,12 +18,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        
-
     // Moves the car foward based on vertical input
     
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       // transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
 
     // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
